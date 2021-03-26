c32890b0fb5c3731c6d8ad65b3279fb9895c540f
Veronika Markova <veronica.mkv11@gmail.com>
2021-03-26 22:25:12 +0400

    first commit

---
+++ H4K0R-News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  H4K0R-News
+//
+//  Created by Veronica Markova on 3/26/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ H4K0R-News/H4K0R_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4K0R_NewsApp.swift
+//  H4K0R-News
+//
+//  Created by Veronica Markova on 3/26/21.
+//
+
+import SwiftUI
+
+@main
+struct H4K0R_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
