49834f8facb749ae7d60922f6964af332fab499e
Veronika Markova <veronica.mkv11@gmail.com>
2021-03-30 02:44:12 +0400

    WebView added.

--- H4K0R-News/Views/DetailView.swift
+++ H4K0R-News/Views/DetailView.swift
@@ -6,7 +6,7 @@
 //
 
 import SwiftUI
-import WebKit
+
 
 struct DetailView: View {
     
@@ -22,21 +22,3 @@ struct DetailView_Previews: PreviewProvider {
     }
 }
 
-struct WebView: UIViewRepresentable {
-    
-    let urlString: String?
-    
-    func makeUIView(context: Context) -> WebView.UIViewType {
-        return WKWebView()
-    }
-    func updateUIView(_ uiView: WKWebView, context: Context) {
-        
-            if let safeString = urlString{
-            if let url = URL(string: safeString){
-                let request = URLRequest(url: url)
-                uiView.load(request)
-            }
-        }
-    }
-
-}
---
+++ H4K0R-News/Views/WebView.swift
@@ -0,0 +1,30 @@
+//
+//  WebView.swift
+//  H4K0R-News
+//
+//  Created by Veronica Markova on 3/28/21.
+//
+
+import Foundation
+import WebKit
+import SwiftUI
+
+
+struct WebView: UIViewRepresentable {
+    
+    let urlString: String?
+    
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        
+            if let safeString = urlString{
+            if let url = URL(string: safeString){
+                let request = URLRequest(url: url)
+                uiView.load(request)
+            }
+        }
+    }
+
+}
